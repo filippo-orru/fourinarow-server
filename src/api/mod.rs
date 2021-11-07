e9ce01430dc803cfb847d09c2bf9addc85d16774
Filippo Orru <filippoorru@hotmail.com>
2021-11-07 11:19:34 +0100

    Debugging: log headers when trying to get session_token

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -78,6 +78,7 @@ pub enum ApiError {
 }
 
 pub fn get_session_token(req: &HttpRequest) -> Option<SessionToken> {
+    println!("{:?}", req.headers());
     req.headers()
         .get("session_token")
         .map(|s| s.to_str().ok().map(|s| SessionToken::parse(s)))
